bfb1c33454e58ed97c9e349046641cff36950b1f
Ximo <liang615@gmail.com>
2019-08-10 22:56:22 +0800

    Merge branch 'master' of https://github.com/XiaMiLang/Golang-study
    # Conflicts:
    #	README.md

---
+++ 01_Go-Fundamental.go
@@ -0,0 +1,41 @@
+//main 函數只能出現在 package main 內
+//main 包才有可執行程序
+package main
+
+//關鍵字 import 導入其它包
+//也可寫成 import aa "fmt", main函數內就會變寫成 aa.Println()
+//也可寫成 import . "fmt", main函數內就會變寫成 Println()
+/*
+import(
+	"errors"
+	"io"
+	"os"
+)
+*/
+import "fmt"
+
+//關鍵字 const 定義常量
+const PI = 3.14
+
+//關鍵字 var 定義全局變量, 在整個 package 當中使用
+var name = "John"
+var Age int
+
+//關鍵字 type 定義類型
+type AA int
+type bb AA
+
+//關鍵字 type 定義結構(struct)
+type student struct {
+}
+
+//關鍵字 type 定義接口(interface)
+type golang interface {
+}
+
+//關鍵字 func 定義函數
+
+//
+func main() {
+	fmt.Println()
+}
