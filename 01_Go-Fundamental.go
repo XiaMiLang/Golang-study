ebb8f5fcbda028294b9fb9144879d6601050cf44
XiaMilang <n97071159@gs.ncku.edu.tw>
2019-08-11 11:44:26 +0800

    1

--- 01_Go-Fundamental.go
+++ 01_Go-Fundamental.go
@@ -16,14 +16,30 @@ import "fmt"
 
 //關鍵字 const 定義常量
 const PI = 3.14
+const (
+	const1 = "1"
+	const2 = 2
+	const3 = 3
+)
 
 //關鍵字 var 定義全局變量, 在整個 package 當中使用
 var name = "John"
 var Age int
+var (
+	name1 = "1"
+	name2 = 2
+	name3 = 3
+)
 
 //關鍵字 type 定義類型
 type AA int
 type bb AA
+type (
+	newType int
+	type1   float32
+	type2   string
+	type3   byte
+)
 
 //關鍵字 type 定義結構(struct)
 type student struct {
@@ -37,3 +53,8 @@ type golang interface {
 func main() {
 	fmt.Println()
 }
+
+/*可見性規則
+Go語言中, 使用 大小寫 來決定該 常量, 變量, 類型, 接口, 結構, 函數 是否可以被外部包所調用
+根據約定, 函數名首字母小寫　即為 private, 大寫 即為 public
+*/
