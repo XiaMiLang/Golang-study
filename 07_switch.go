895eb69a61ae42d4166a1a38d43969e9dea48a00
Ximo <liang615@gmail.com>
2019-08-24 18:04:28 +0800

    20190824

--- 07_switch.go
+++ 07_switch.go
@@ -1,1 +1,55 @@
 package main
+
+import "fmt"
+
+//可以使用任何類型或表達式作為條件問句"
+//不需 break, 一旦條件符合自動終止
+//如需繼續執行下去, 使用 fallthrough 語句
+//支持初始化表達式
+
+//func main(){
+//	a :=1
+//	switch a {
+//	case 0:
+//		fmt.Println("a=0")
+//	case 1:
+//		fmt.Println("a=1")
+//	case 2:
+//		fmt.Println("a=2")
+//	default:
+//		fmt.Println("no found")
+//
+//	}
+//}
+
+//func main(){
+//	a :=1
+//	switch {
+//	case a>=0:
+//		fmt.Println("a=0")
+//		fallthrough
+//	case a>=1:
+//		fmt.Println("a=1")
+//	case a>=2:
+//		fmt.Println("a=2")
+//	default:
+//		fmt.Println("no found")
+//
+//	}
+//}
+
+//初始化語句放在 switch 後面
+func main(){
+	switch a :=1; {
+	case a>=0:
+		fmt.Println("a=0")
+		fallthrough
+	case a>=1:
+		fmt.Println("a=1")
+	case a>=2:
+		fmt.Println("a=2")
+	default:
+		fmt.Println("no found")
+
+	}
+}
---
+++ 08_goto-break-continue.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+//三個語法都可配合標籤使用
+//標籤區分大小寫, 標籤放了不用會造成編譯錯誤
+//標籤盡量放在 goto 之後以避免死循環
+//func main(){
+//	LABEL1:
+//		for {
+//			for i:=0; i<10; i++{
+//				if i >3{
+//					fmt.Println(i)
+//					break LABEL1
+//				}
+//			}
+//		}
+//		fmt.Println("ok")
+//}
+
+//goto :調整程序執行的位置
+//func main() {
+//LABEL1:
+//	fmt.Println("start")
+//	for a:=1;a<10;a++{
+//		for i := 0; i < 10; i++ {
+//			if i > 3 {
+//				fmt.Println(i)
+//				goto LABEL2
+//			}
+//		}
+//	}
+//
+//	fmt.Println("ok")
+//
+//LABEL2:
+//	fmt.Println("LABEL2")
+//	goto LABEL1
+//
+//}
+
+//continue 跳過該次循環的所有代碼, 繼續下次循環
+//func main(){
+//	for i:=1; i<=10; i++{
+//		if i==5{
+//			continue
+//		}
+//		fmt.Println(i)
+//	}
+//}
+
+//func main(){
+//LABEL1:
+//	for i :=0; i<10;i++{
+//		for{
+//			fmt.Println(i)
+//			continue LABEL1
+//			fmt.Println("會執行嗎?",i)
+//		}
+//	}
+//	fmt.Println("ok")
+//}
+
+func main(){
+LABEL1:
+	for i :=0; i<10;i++{
+		for{
+			fmt.Println(i)
+			continue LABEL1  //作業, 如果 continue 換成 goto, 結果會一樣嗎?
+			fmt.Println("會執行嗎?",i)
+		}
+	}
+	fmt.Println("ok")
+}
